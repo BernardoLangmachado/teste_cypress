2dfb0b5ae3cf35b6ff6dc919de41ded5da4cd99e
BernardoLangmachado <130462210+BernardoLangmachado@users.noreply.github.com>
2024-09-26 11:57:26 -0300

    cadastro atualizado

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,27 +1,28 @@
-describe('pagina de login',() => {
+describe('pagina de login', () => {
 
   it('Login com sucesso', () => {
-    cy.visit('http://127.0.0.1:5500/login');
+    cy.visit('http://localhost:63342/pg-login-testes-3C/index.html?_ijt=mc92nqhu83ng6e1pderfjdeblu');
 
-    //prencher os campos de login e senha
+    //preencher os campos de usuario e senha
     cy.get('#usuario').type('admin');
     cy.get('#senha').type('admin');
-    cy.get('#entrar').click();
+    cy.get('#entrar').click()
 
-    cy.on('window:alert',(txt) => {
-      expect(txt).to.contains('Login feito');
+    cy.on('window:alert', (txt) =>{
+      expect(txt).to.contains('Login efetuado!')
     });
 
   });
 
-  it('Login com erro',() =>{
-    cy.visit('http://127.0.0.1:5500/login');
+  it('Login com erro', () => {
+    cy.visit('http://localhost:63342/pg-login-testes-3C/index.html?_ijt=mc92nqhu83ng6e1pderfjdeblu');
 
-    cy.get('#usuario').type('12345');
+    cy.get('#usuario').type('1234');
     cy.get('#senha').type('errado');
     cy.get('#entrar').click()
 
-    cy.get('#error').should('be.visible')
+    cy.get('#erro').should('be.visible')
+
   });
 
 });
